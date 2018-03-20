aca03c6b8457fd9207f3eda9081220f222dffa2a
David Naranjo <dnaranjo89@gmail.com>
2018-03-20 22:17:25 +0100

    Clean PublishDrawOptions

--- src/components/PublishDrawOptions/PublishDrawOptions.jsx
+++ src/components/PublishDrawOptions/PublishDrawOptions.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'material-ui/Button';
-import Grid from 'material-ui/Grid';
 
 const PublishDrawOptions = props => (
-  <Button raised color="primary" onClick={props.handlePublish}>
-    {props.label_publish}
-  </Button>
+  <div>
+    {/* TODO do stuff here about inmediate publish or scheduled draw */}
+    <Button raised color="primary" onClick={props.handlePublish}>
+      {props.label_publish}
+    </Button>
+  </div>
 );
 
 PublishDrawOptions.propTypes = {
   label_publish: PropTypes.string.isRequired,
-  label_schedule: PropTypes.string.isRequired,
   handlePublish: PropTypes.func.isRequired,
 };
 
