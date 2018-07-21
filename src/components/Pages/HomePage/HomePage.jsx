9e8bf48669af21aa8ba2176dc24c0aff9527d534
David Naranjo <dnaranjo89@gmail.com>
2018-07-21 05:26:12 +0200

    Clean Homepage

--- src/components/Pages/HomePage/HomePage.jsx
+++ src/components/Pages/HomePage/HomePage.jsx
@@ -1,44 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { translate } from 'react-i18next';
-import Grid from '@material-ui/core/Grid';
+import classNames from 'classnames/bind';
 
 import Chip from './../../DrawCard/DrawCard';
 import TransparentPanel from '../../TransparentPanel/TransparentPanel';
 import Page from '../../Page/Page';
 import rafflesIcon from './raffles.png';
+import randomNumber from './random_number.png';
+import randomLetter from './random_letter.png';
+
+import STYLES from './HomePage.scss';
+
+const c = classNames.bind(STYLES);
 
 const HomePage = ({ t }) => (
   <Page htmlTitle={'Échalo A Suerte'}>
-    <Grid container justify="center">
-      <Grid item sm={8}>
-        <TransparentPanel>
-          <Grid container justify="center">
-            <Grid item sm={6}>
-              <Chip
-                icon="https://echaloasuerte.com/static/img/draw_icons/random_number.dd452a269894.png"
-                href="/number"
-              >
-                {t('random_number_title')}
-              </Chip>
-            </Grid>
-            <Grid item sm={6}>
-              <Chip
-                icon="https://echaloasuerte.com/static/img/draw_icons/random_letter.07b9689f39d4.png"
-                href="/letter"
-              >
-                {t('random_letter_title')}
-              </Chip>
-            </Grid>
-            <Grid item sm={6}>
-              <Chip icon={rafflesIcon} href="/raffles">
-                {t('raffle_section_title')}
-              </Chip>
-            </Grid>
-          </Grid>
-        </TransparentPanel>
-      </Grid>
-    </Grid>
+    <TransparentPanel className={c('HomePage__container')}>
+      <div className={c('HomePage__draw-chips-list')}>
+        <Chip icon={randomNumber} href="/number">
+          {t('random_number_title')}
+        </Chip>
+        <Chip icon={randomLetter} href="/letter">
+          {t('random_letter_title')}
+        </Chip>
+        <Chip icon={rafflesIcon} href="/raffles">
+          {t('raffle_section_title')}
+        </Chip>
+      </div>
+    </TransparentPanel>
   </Page>
 );
 
--- src/components/Pages/RafflesSectionPage/RafflesSectionPage.jsx
+++ src/components/Pages/RafflesSectionPage/RafflesSectionPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { translate } from 'react-i18next';
 import Grid from '@material-ui/core/Grid';
+import classNames from 'classnames/bind';
 
 import Chip from './../../DrawCard/DrawCard';
 import TransparentPanel from '../../TransparentPanel/TransparentPanel';
@@ -10,31 +11,25 @@ import likeIcon from './like.jpg';
 import facebookIcon from './facebook.png';
 import raffleIcon from './raffle.png';
 
+import STYLES from './RafflesSectionPage.scss';
+
+const c = classNames.bind(STYLES);
+
 const RafflesSectionPage = ({ t }) => (
   <Page htmlTitle={'Échalo A Suerte'}>
-    <Grid container justify="center">
-      <Grid item sm={8}>
-        <TransparentPanel>
-          {/* <Grid container justify="center"> */}
-          <Grid item sm={6}>
-            <Chip icon={facebookIcon} href="/facebook_login">
-              {t('facebook_login_raffle_title')}
-            </Chip>
-          </Grid>
-          <Grid item sm={6}>
-            <Chip icon={likeIcon} href="/facebook_photo">
-              {t('facebook_photo_raffle_title')}
-            </Chip>
-          </Grid>
-          <Grid item sm={6}>
-            <Chip icon={raffleIcon} href="/raffle">
-              {t('raffle_title')}
-            </Chip>
-          </Grid>
-          {/* </Grid> */}
-        </TransparentPanel>
-      </Grid>
-    </Grid>
+    <TransparentPanel className={c('RafflesSectionPage__container')}>
+      <div className={c('RafflesSectionPage__draw-chips-list')}>
+        <Chip icon={facebookIcon} href="/facebook_login">
+          {t('facebook_login_raffle_title')}
+        </Chip>
+        <Chip icon={likeIcon} href="/facebook_photo">
+          {t('facebook_photo_raffle_title')}
+        </Chip>
+        <Chip icon={raffleIcon} href="/raffle">
+          {t('raffle_title')}
+        </Chip>
+      </div>
+    </TransparentPanel>
   </Page>
 );
 
