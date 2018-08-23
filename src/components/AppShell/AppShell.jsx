dd7b4564c32f27e6851e262b96b5daae3b0041ed
David Naranjo <dnaranjo89@gmail.com>
2018-08-23 20:59:13 +0200

    Remove DrawPage

--- src/components/AppShell/AppShell.jsx
+++ src/components/AppShell/AppShell.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import classnames from 'classnames/bind';
 
 import HomePage from '../Pages/HomePage/HomePage';
 import AboutPage from '../Pages/AboutPage/AboutPage';
-import DrawPage, { urls } from '../Pages/DrawPage/DrawPage';
 import Header from '../Header/Header';
 import RafflesSectionPage from '../Pages/RafflesSectionPage/RafflesSectionPage';
 import Footer from '../Footer/Footer';
 import Typographies from '../Pages/Typographies/Typographies';
 import STYLES from './AppShell.scss';
 
-const c = className => STYLES[className];
+import PublishedRandomNumberPageContainer from '../Pages/RandomNumber/PublishedRandomNumberPageContainer';
+import RafflePageContainer from '../Pages/RafflePage/RafflePageContainer';
+import PublishedRafflePageContainer from '../Pages/RafflePage/PublishedRafflePageContainer';
+import RandomNumberPageContainer from '../Pages/RandomNumber/RandomNumberPageContainer';
+import FacebookLoginRafflePageContainer from '../Pages/FacebookLoginRaffle/FacebookLoginRafflePageContainer';
+import PublishedFacebookLoginRafflePageContainer from '../Pages/FacebookLoginRaffle/PublishedFacebookLoginRafflePageContainer';
+import FacebookPhotoRafflePageContainer from '../Pages/FacebookPhotoRaffle/FacebookPhotoRafflePageContainer';
+import PublishedFacebookPhotoRafflePageContainer from '../Pages/FacebookPhotoRaffle/PublishedFacebookPhotoRafflePageContainer';
+import LetterDrawPageContainer from '../Pages/LetterDrawPage/LetterDrawPageContainer';
+import SpinArrowPageContainer from '../Pages/SpinArrowPage/SpinArrowPageContainer';
+
+const c = classnames.bind(STYLES);
 
 const AppShell = () => (
   <div className={c('AppShell')}>
@@ -21,7 +32,28 @@ const AppShell = () => (
         <Route exact path="/about" component={AboutPage} />
         <Route exact path="/typography" component={Typographies} />
         <Route exact path="/raffles" component={RafflesSectionPage} />
-        <Route path={urls} component={props => <DrawPage {...props} />} />
+
+        <Route exact path="/number" component={RandomNumberPageContainer} />
+        <Route exact path="/number/:drawId" component={PublishedRandomNumberPageContainer} />
+
+        <Route exact path="/raffle" component={props => <RafflePageContainer {...props} />} />
+        <Route path="/raffle/:drawId" component={PublishedRafflePageContainer} />
+
+        <Route exact path="/arrow" component={SpinArrowPageContainer} />
+        <Route exact path="/facebook_photo" component={FacebookPhotoRafflePageContainer} />
+        <Route
+          path="/facebook_photo/:drawId"
+          component={PublishedFacebookPhotoRafflePageContainer}
+        />
+
+        <Route exact path="/facebook_login" component={FacebookLoginRafflePageContainer} />
+        <Route
+          path="/facebook_login/:drawId"
+          component={PublishedFacebookLoginRafflePageContainer}
+        />
+
+        <Route exact path="/letter" component={LetterDrawPageContainer} />
+
         <Route render={() => <div>Not found</div>} />
       </Switch>
     </div>
