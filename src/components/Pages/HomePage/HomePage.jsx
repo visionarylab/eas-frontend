d240f73117b796646981bed97ff9be253bfe4fd1
David Naranjo <dnaranjo89@gmail.com>
2018-09-21 19:40:04 +0200

    Add the rest of the draws pending to be migrated

--- src/components/Pages/HomePage/HomePage.jsx
+++ src/components/Pages/HomePage/HomePage.jsx
@@ -12,6 +12,9 @@ import arrowIcon from './arrow.svg';
 import randomLetter from './random_letter.png';
 import groupsIcon from './groups.png';
 import coinIcon from './coin.svg';
+import diceIcon from './dice.svg';
+import cardsIcon from './cards.svg';
+import tournamentIcon from './tournament.png';
 
 import STYLES from './HomePage.scss';
 
@@ -22,7 +25,7 @@ const HomePage = ({ t }) => (
     <Typography variant="display1">Sorteos online</Typography>
     <div className={c('HomePage__draw-chips-group')}>
       <DrawCard icon={rafflesIcon} to="/raffles">
-        {t('raffle_section_title')}
+        {t('draw_title_raffle_section')}
       </DrawCard>
       <DrawCard icon={groupsIcon} to="/groups">
         Crear equipos
@@ -31,20 +34,32 @@ const HomePage = ({ t }) => (
     <Typography variant="display1">Generadores básicos al azar</Typography>
     <div className={c('HomePage__draw-chips-group')}>
       <DrawCard icon={randomNumber} to="/number">
-        {t('random_number_title')}
+        {t('draw_title_random_number')}
       </DrawCard>
       <DrawCard icon={randomLetter} to="/letter">
-        {t('random_letter_title')}
+        {t('draw_title_random_letter')}
       </DrawCard>
     </div>
     <Typography variant="display1">Otros</Typography>
     <div className={c('HomePage__draw-chips-group')}>
       <DrawCard icon={arrowIcon} to="/arrow">
-        {t('spin_arrow_title')}
+        {t('draw_title_spin_arrow')}
       </DrawCard>
+    </div>
 
+    <Typography variant="display1">Not migrated yet</Typography>
+    <div className={c('HomePage__draw-chips-group')}>
       <DrawCard icon={coinIcon} externalHref="https://echaloasuerte.com/draw/new/coin/">
-        {t('flip_coin_title')}
+        {t('draw_title_flip_coin')}
+      </DrawCard>
+      <DrawCard icon={diceIcon} externalHref="https://echaloasuerte.com/draw/new/dice/">
+        {t('draw_title_roll_dice')}
+      </DrawCard>
+      <DrawCard icon={cardsIcon} externalHref="https://echaloasuerte.com/draw/new/cards/">
+        {t('draw_title_pick_card')}
+      </DrawCard>
+      <DrawCard icon={tournamentIcon} externalHref="https://echaloasuerte.com/draw/new/tournament/">
+        {t('draw_title_tournament')}
       </DrawCard>
     </div>
   </Page>
