56094fb00d0bc212fdfe91aa31459fb1d34d6854
David Naranjo <dnaranjo89@gmail.com>
2018-10-15 19:46:29 +0200

    Change brand name to Woreep for the english site

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import Typography from '@material-ui/core/Typography';
+import { translate } from 'react-i18next';
 
 import STYLES from './Header.scss';
 import logo from './logo_vector2.svg';
 
 const c = classNames.bind(STYLES);
 
-const Header = () => (
+const Header = ({ t }) => (
   <header className={c('Header')}>
     <Link to={'/'} className={c('Header__link')}>
-      <img className={c('Header__logo')} src={logo} alt="EchaloASuerte" />
+      <img className={c('Header__logo')} src={logo} alt={t('brand_name')} />
       <Typography variant={'display4'} className={c('Header__title')} component="span">
-        EchaloASuerte
+        {t('brand_name')}
       </Typography>
     </Link>
   </header>
 );
 
-export default Header;
+Header.propTypes = {
+  t: PropTypes.func.isRequired,
+};
+
+export default translate('Header')(Header);
