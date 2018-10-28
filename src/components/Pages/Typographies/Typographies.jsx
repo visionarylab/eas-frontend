f2ac6a2aa479c11718e3249de733f306df98c912
David Naranjo <dnaranjo89@gmail.com>
2018-10-28 12:39:59 +0100

    Update typography page

--- src/components/Pages/Typographies/Typographies.jsx
+++ src/components/Pages/Typographies/Typographies.jsx
@@ -4,16 +4,30 @@ import Typography from '@material-ui/core/Typography';
 const Typographies = () => (
   <div>
     <br />
-    <Typography variant="h1">Display 1 - (draw titles)</Typography>
+    <Typography variant="h1">h1 - Main page title</Typography>
     <br />
-    <Typography variant="body1" color={'textSecondary'}>
-      body1 - (draw subtitle)
-    </Typography>
+    <Typography variant="h2">h2 - Section title</Typography>
     <br />
-    <Typography variant="title">Title (Section titles)</Typography>
+    <Typography variant="h3">h3 - Section title (for draw creation)</Typography>
     <br />
-    <Typography variant="subheading">Subheading (Draw card title)</Typography>
+    <Typography variant="h5">h5 - Published draw details</Typography>
     <br />
+    <Typography variant="body1">body1 - Some normal text</Typography>
+    <br />
+    <Typography variant="body2">body2 - Some normal text, but smaller</Typography>
+    <br />
+    <Typography variant="subtitle1">subtitle1 - Description in draw pages </Typography>
+    <br />
+    <br />
+    <br />
+    <br />
+    <Typography variant="h2">h2 - Not used</Typography>
+    <br />
+    <Typography variant="h4">h4 - Not used</Typography>
+    <br />
+    <Typography variant="h6">h6 - Not used</Typography>
+    <br />
+    <Typography variant="title">Title - Not used</Typography>
   </div>
 );
 
