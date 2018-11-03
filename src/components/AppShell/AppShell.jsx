2f177acbfae57ea6ea329419d1f5b72a2e99a9d9
David Naranjo <dnaranjo89@gmail.com>
2018-11-03 10:03:43 +0100

    Comment out code that is not ready for prod

--- src/components/AppShell/AppShell.jsx
+++ src/components/AppShell/AppShell.jsx
@@ -8,6 +8,7 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Typographies from '../Pages/Typographies/Typographies';
 import STYLES from './AppShell.scss';
+import config from '../../config/config';
 
 import PublishedRandomNumberPageContainer from '../Pages/RandomNumber/PublishedRandomNumberPageContainer';
 import PublishedGroupsGeneratorPageContainer from '../Pages/GroupsGenerator/PublishedGroupsGeneratorPageContainer';
@@ -15,11 +16,11 @@ import RafflePageContainer from '../Pages/RafflePage/RafflePageContainer';
 import PublishedRafflePageContainer from '../Pages/RafflePage/PublishedRafflePageContainer';
 import RandomNumberPageContainer from '../Pages/RandomNumber/RandomNumberPageContainer';
 import GroupsGeneratorPageContainer from '../Pages/GroupsGenerator/GroupsGeneratorPageContainer';
-import FacebookLoginRafflePageContainer from '../Pages/FacebookLoginRaffle/FacebookLoginRafflePageContainer';
-import PublishedFacebookLoginRafflePageContainer from '../Pages/FacebookLoginRaffle/PublishedFacebookLoginRafflePageContainer';
-import FacebookPhotoRafflePageContainer from '../Pages/FacebookPhotoRaffle/FacebookPhotoRafflePageContainer';
-import PublishedFacebookPhotoRafflePageContainer from '../Pages/FacebookPhotoRaffle/PublishedFacebookPhotoRafflePageContainer';
-import LetterDrawPageContainer from '../Pages/LetterDrawPage/LetterDrawPageContainer';
+// import FacebookLoginRafflePageContainer from '../Pages/FacebookLoginRaffle/FacebookLoginRafflePageContainer';
+// import PublishedFacebookLoginRafflePageContainer from '../Pages/FacebookLoginRaffle/PublishedFacebookLoginRafflePageContainer';
+// import FacebookPhotoRafflePageContainer from '../Pages/FacebookPhotoRaffle/FacebookPhotoRafflePageContainer';
+// import PublishedFacebookPhotoRafflePageContainer from '../Pages/FacebookPhotoRaffle/PublishedFacebookPhotoRafflePageContainer';
+// import LetterDrawPageContainer from '../Pages/LetterDrawPage/LetterDrawPageContainer';
 import SpinArrowPageContainer from '../Pages/SpinArrowPage/SpinArrowPageContainer';
 import FlipCoinPageContainer from '../Pages/FlipCoinPage/FlipCoinPageContainer';
 
@@ -30,9 +31,6 @@ const AppShell = () => (
     <Header />
     <Switch>
       <Route exact path="/" component={props => <HomePage {...props} />} />
-      <Route exact path="/about" component={AboutPage} />
-      <Route exact path="/typography" component={Typographies} />
-
       <Route exact path="/groups" component={GroupsGeneratorPageContainer} />
       <Route
         exact
@@ -52,18 +50,20 @@ const AppShell = () => (
       <Route exact path="/raffle" component={props => <RafflePageContainer {...props} />} />
       <Route path="/raffle/:drawId" component={PublishedRafflePageContainer} />
 
-      <Route exact path="/facebook_photo" component={FacebookPhotoRafflePageContainer} />
-      <Route path="/facebook_photo/:drawId" component={PublishedFacebookPhotoRafflePageContainer} />
-
-      <Route exact path="/facebook_login" component={FacebookLoginRafflePageContainer} />
-      <Route path="/facebook_login/:drawId" component={PublishedFacebookLoginRafflePageContainer} />
-
-      <Route exact path="/letter" component={LetterDrawPageContainer} />
-
       <Route exact path="/flip-a-coin" component={FlipCoinPageContainer} />
-
       <Route exact path="/arrow" component={SpinArrowPageContainer} />
 
+      {/* <Route exact path="/facebook_photo" component={FacebookPhotoRafflePageContainer} />
+      <Route path="/facebook_photo/:drawId" component={PublishedFacebookPhotoRafflePageContainer} />
+      <Route exact path="/facebook_login" component={FacebookLoginRafflePageContainer} />
+      <Route path="/facebook_login/:drawId" component={PublishedFacebookLoginRafflePageContainer} />
+      <Route exact path="/letter" component={LetterDrawPageContainer} /> */}
+
+      <Route exact path="/about" component={AboutPage} />
+
+      {config.environment === 'local' && (
+        <Route exact path="/typography" component={Typographies} />
+      )}
       <Route render={() => <div>Not found</div>} />
     </Switch>
     <Footer />
--- src/components/Pages/HomePage/HomePage.jsx
+++ src/components/Pages/HomePage/HomePage.jsx
@@ -7,57 +7,49 @@ import DrawCard from './../../DrawCard/DrawCard';
 import Page from '../../Page/Page';
 import randomNumber from './random_number.png';
 import arrowIcon from './arrow.svg';
-import randomLetter from './random_letter.png';
 import groupsIcon from './groups.png';
 import coinIcon from './coin.svg';
-import diceIcon from './dice.svg';
-import cardsIcon from './cards.svg';
-import tournamentIcon from './tournament.png';
-import likeIcon from './like.jpg';
-import facebookIcon from './facebook.png';
+// import randomLetter from './random_letter.png';
+// import diceIcon from './dice.svg';
+// import cardsIcon from './cards.svg';
+// import tournamentIcon from './tournament.png';
+// import likeIcon from './like.jpg';
+// import facebookIcon from './facebook.png';
 import raffleIcon from './raffle.svg';
 
 import STYLES from './HomePage.scss';
 
 const c = classNames.bind(STYLES);
 
-const OldHomepage = ({ t }) => (
-  <div className={c('HomePage__container')}>
-    <Typography variant="h1">Sorteos online</Typography>
+const HomePage = ({ t }) => (
+  <Page htmlTitle={'Échalo A Suerte'} className={c('HomePage')}>
+    <div className={c('HomePage__container')}>
+      <Typography variant="h1">{t('section_title_online_raffles')}</Typography>
+      <div className={c('HomePage__draw-chips-group')}>
+        <DrawCard icon={raffleIcon} to="/raffle">
+          {t('draw_title_raffle')}
+        </DrawCard>
+        <DrawCard icon={groupsIcon} to="/groups">
+          {t('draw_title_groups_generator')}
+        </DrawCard>
+        <DrawCard icon={randomNumber} to="/number">
+          {t('draw_title_random_number')}
+        </DrawCard>
+      </div>
+      <Typography variant="h1"> {t('section_title_simple_draws')}</Typography>
+      <div className={c('HomePage__draw-chips-group')}>
+        <DrawCard icon={coinIcon} to="flip-a-coin">
+          {t('draw_title_flip_coin')}
+        </DrawCard>
+        <DrawCard icon={arrowIcon} to="/arrow">
+          {t('draw_title_spin_arrow')}
+        </DrawCard>
+      </div>
+
+      {/* <Typography variant="h1">Not migrated yet</Typography>
     <div className={c('HomePage__draw-chips-group')}>
-      <DrawCard icon={facebookIcon} to="/facebook_login">
-        {t('draw_title_facebook_login')}
-      </DrawCard>
-      <DrawCard icon={likeIcon} to="/facebook_photo">
-        {t('draw_title_facebook_photo')}
-      </DrawCard>
-      <DrawCard icon={raffleIcon} to="/raffle">
-        {t('draw_title_raffle')}
-      </DrawCard>
-    </div>
-    <Typography variant="h1">Generadores básicos al azar</Typography>
-    <div className={c('HomePage__draw-chips-group')}>
-      <DrawCard icon={randomNumber} to="/number">
-        {t('draw_title_random_number')}
-      </DrawCard>
       <DrawCard icon={randomLetter} to="/letter">
         {t('draw_title_random_letter')}
-      </DrawCard>
-    </div>
-    <Typography variant="h1">Otros</Typography>
-    <div className={c('HomePage__draw-chips-group')}>
-      <DrawCard icon={arrowIcon} to="/arrow">
-        {t('draw_title_spin_arrow')}
-      </DrawCard>
-      <DrawCard icon={groupsIcon} to="/groups">
-        {t('draw_title_groups_generator')}
-      </DrawCard>
-    </div>
-
-    <Typography variant="h1">Not migrated yet</Typography>
-    <div className={c('HomePage__draw-chips-group')}>
-      <DrawCard icon={coinIcon} to="flip-a-coin">
-        {t('draw_title_flip_coin')}
       </DrawCard>
       <DrawCard icon={diceIcon} externalHref="https://echaloasuerte.com/draw/new/dice/">
         {t('draw_title_roll_dice')}
@@ -68,47 +60,14 @@ const OldHomepage = ({ t }) => (
       <DrawCard icon={tournamentIcon} externalHref="https://echaloasuerte.com/draw/new/tournament/">
         {t('draw_title_tournament')}
       </DrawCard>
+      <DrawCard icon={facebookIcon} to="/facebook_login">
+        {t('draw_title_facebook_login')}
+      </DrawCard>
+      <DrawCard icon={likeIcon} to="/facebook_photo">
+        {t('draw_title_facebook_photo')}
+      </DrawCard>
+    </div> */}
     </div>
-  </div>
-);
-
-OldHomepage.propTypes = {
-  t: PropTypes.func.isRequired,
-};
-
-const WoreepHomepage = ({ t }) => (
-  <div className={c('HomePage__container')}>
-    <Typography variant="h1">{t('section_title_online_raffles')}</Typography>
-    <div className={c('HomePage__draw-chips-group')}>
-      <DrawCard icon={raffleIcon} to="/raffle">
-        {t('draw_title_raffle')}
-      </DrawCard>
-      <DrawCard icon={groupsIcon} to="/groups">
-        {t('draw_title_groups_generator')}
-      </DrawCard>
-      <DrawCard icon={randomNumber} to="/number">
-        {t('draw_title_random_number')}
-      </DrawCard>
-    </div>
-    <Typography variant="h1"> {t('section_title_simple_draws')}</Typography>
-    <div className={c('HomePage__draw-chips-group')}>
-      <DrawCard icon={coinIcon} to="flip-a-coin">
-        {t('draw_title_flip_coin')}
-      </DrawCard>
-      <DrawCard icon={arrowIcon} to="/arrow">
-        {t('draw_title_spin_arrow')}
-      </DrawCard>
-    </div>
-  </div>
-);
-
-WoreepHomepage.propTypes = {
-  t: PropTypes.func.isRequired,
-};
-
-const HomePage = ({ t }) => (
-  <Page htmlTitle={'Échalo A Suerte'} className={c('HomePage')}>
-    <WoreepHomepage t={t} />
   </Page>
 );
 
