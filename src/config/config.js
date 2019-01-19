48dd97b6a05b1f92dd75f9c8d0a78005ec4577b0
David Naranjo <dnaranjo89@gmail.com>
2019-01-20 00:16:04 +0100

    Sort out config files

--- src/config/config.js
+++ src/config/config.js
@@ -1,29 +1,41 @@
-function isServer() {
-  return !(typeof window !== 'undefined' && window.document);
-}
+/* eslint-disable no-console */
+/* eslint-disable no-underscore-dangle */
+
+const isServer = !(typeof window !== 'undefined' && window.document);
 
 let config = {};
-try {
-  const environment = process.env.REACT_APP_ENV;
-  if (environment !== 'production') {
-    console.log(`Loading application config for environment: ${environment}`); // eslint-disable-line no-console
+let environment;
+
+if (isServer) {
+  environment = process.env.EAS_APP_ENV;
+} else if (window && window.__internal) {
+  environment = window.__internal.ENVIRONMENT;
+}
+
+if (environment) {
+  try {
+    if (environment !== 'production') {
+      console.log(`Loading application config for environment: ${environment}`);
+    }
+
+    const baseConfig = {
+      environment,
+      domain: 'https://echaloasuerte.com',
+      isServer,
+    };
+
+    const environmentConfig = require(`./${environment}`).default; // eslint-disable-line 
+
+    config = Object.assign({}, baseConfig, environmentConfig);
+
+    // Disable logs and events when rendering in server
+    config.googleAnaliticsEnabled = !isServer && config.googleAnaliticsEnabled;
+    config.sentryEnabled = !isServer && config.sentryEnabled;
+  } catch (e) {
+    console.error('No application config could be found.', e);
   }
-
-  const baseConfig = {
-    environment,
-    domain: 'https://echaloasuerte.com',
-    isServer: isServer(),
-  };
-
-  const environmentConfig = require(`./${environment}`).default; // eslint-disable-line
-
-  config = Object.assign({}, baseConfig, environmentConfig);
-
-  // Disable logs and events when rendering in server
-  config.googleAnaliticsEnabled = !isServer && config.googleAnaliticsEnabled;
-  config.sentryEnabled = !isServer && config.sentryEnabled;
-} catch (e) {
-  console.log('No application config could be found.', e); // eslint-disable-line no-console
+} else {
+  console.error('No environment specified. Please set the EAS_APP_ENV environment variable');
 }
 
 module.exports = config;
