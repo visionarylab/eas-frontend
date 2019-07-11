aabf83eb347780ea8651d81b2760175bc2fa8aa3
David Naranjo <dnaranjo89@gmail.com>
2019-07-11 19:57:04 +0200

    Fix description layout in published groups draw

--- src/components/DrawHeading/DrawHeading.jsx
+++ src/components/DrawHeading/DrawHeading.jsx
@@ -8,9 +8,11 @@ const c = classnames.bind(STYLES);
 
 const DrawHeading = ({ title, subtitle }) => (
   <div className={c('DrawHeading')}>
-    <Typography variant="h1">{title}</Typography>
+    <Typography align="center" variant="h1">
+      {title}
+    </Typography>
     {subtitle && (
-      <Typography variant="subtitle1" className={c('DrawHeading__subtitle')}>
+      <Typography align="center" variant="subtitle1" className={c('DrawHeading__subtitle')}>
         {subtitle}
       </Typography>
     )}
