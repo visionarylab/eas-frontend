02b3771b4add1e2e1e120cbae0683d032f95132b
David Naranjo <dnaranjo89@gmail.com>
2019-07-11 21:42:20 +0200

    Fix integration tests for groups draw

--- src/components/DrawHeading/DrawHeading.jsx
+++ src/components/DrawHeading/DrawHeading.jsx
@@ -8,11 +8,16 @@ const c = classnames.bind(STYLES);
 
 const DrawHeading = ({ title, subtitle }) => (
   <div className={c('DrawHeading')}>
-    <Typography align="center" variant="h1">
+    <Typography align="center" variant="h1" data-testid="DrawHeading__title">
       {title}
     </Typography>
     {subtitle && (
-      <Typography align="center" variant="subtitle1" className={c('DrawHeading__subtitle')}>
+      <Typography
+        align="center"
+        variant="subtitle1"
+        className={c('DrawHeading__subtitle')}
+        data-testid="DrawHeading__subtitle"
+      >
         {subtitle}
       </Typography>
     )}
