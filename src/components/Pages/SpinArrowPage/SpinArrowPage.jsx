3eceb827314b2f9188d680695ca997cc2565b8bd
David Naranjo <dnaranjo89@gmail.com>
2019-07-25 19:01:57 +0200

    Add SEO content for arrow draw

--- src/components/Pages/SpinArrowPage/SpinArrowPage.jsx
+++ src/components/Pages/SpinArrowPage/SpinArrowPage.jsx
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
 import classnames from 'classnames/bind';
 import Typography from '@material-ui/core/Typography';
-
+import DrawHeading from '../../DrawHeading/DrawHeading.jsx';
+import LearnMoreSection from '../../LearnMoreSection/LearnMoreSection.jsx';
 import Page from '../../Page/Page.jsx';
 import STYLES from './SpinArrowPage.scss';
 import ArrowImage from './arrow.svg';
@@ -11,7 +12,7 @@ import arrowOgImage from './arrow_og_image.png';
 
 const c = classnames.bind(STYLES);
 
-const SpinArrowPage = ({ angle, animate, handleToss, t }) => {
+const SpinArrowPage = ({ angle, showTossHelp, animate, handleToss, t }) => {
   const arrowCss = {
     transform: `rotate(${angle}deg)`,
   };
@@ -24,13 +25,11 @@ const SpinArrowPage = ({ angle, animate, handleToss, t }) => {
       htmlTitle={t('html_title')}
       htmlDescription={t('html_description')}
       htmlKeywords={t('html_keywords')}
-      pageType="spin_arrow_draw"
+      pageType="Spin Arrow"
       ogImage={arrowOgImage}
       className={c('SpinArrowPage')}
     >
-      <Typography variant="h1">{t('page_title')}</Typography>
-      <Typography variant="subtitle1">{t('draw_subheading')}</Typography>
-
+      <DrawHeading title={t('page_title')} subtitle={t('draw_subheading')} />
       <div className={c('SpinArrowPage__container')}>
         <button type="button" className={c('SpinArrowPage__button')} onClick={handleToss}>
           <img
@@ -41,13 +40,20 @@ const SpinArrowPage = ({ angle, animate, handleToss, t }) => {
             alt="Arrow"
           />
         </button>
+        {showTossHelp && (
+          <Typography variant="subtitle1" align="center" className={c('SpinArrowPage__touch-hint')}>
+            {t('touch_to_toss')}
+          </Typography>
+        )}
       </div>
+      <LearnMoreSection title={t('learn_more_title')} content={t('learn_more_content')} />
     </Page>
   );
 };
 
 SpinArrowPage.propTypes = {
   angle: PropTypes.number.isRequired,
+  showTossHelp: PropTypes.bool.isRequired,
   animate: PropTypes.bool,
   handleToss: PropTypes.func.isRequired,
   t: PropTypes.func.isRequired,
