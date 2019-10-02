b7cfd4ae6cb0fa2858679a92cb333524b6da5f34
David Naranjo <dnaranjo89@gmail.com>
2019-10-02 21:15:32 +0200

    Show ads

--- src/components/Advert/Advert.jsx
+++ src/components/Advert/Advert.jsx
@@ -8,7 +8,7 @@ const c = classnames.bind(STYLES);
 
 const Advert = props => {
   useEffect(() => {
-    if (window && false) (window.adsbygoogle = window.adsbygoogle || []).push({});
+    if (window) (window.adsbygoogle = window.adsbygoogle || []).push({});
   }, []);
 
   const { isMobile } = props;
