cc2d5483fb4cb31e0eafcb128871569da9e5f5d9
David Naranjo <dnaranjo89@gmail.com>
2019-10-26 13:25:42 +0200

    Add max limit of retries in get data after countdown

--- src/hooks/useLoadDataAfterCountdown.js
+++ src/hooks/useLoadDataAfterCountdown.js
@@ -1,14 +1,18 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+const MAX_RETRIES = 5;
 
 export default function useLoadDataAfterCountdown(result, loadData) {
+  const [retries, setRetries] = useState(MAX_RETRIES);
+
   // eslint-disable-next-line consistent-return
   useEffect(() => {
-    if (result && !result.value) {
+    if (result && !result.value && retries > 0) {
+      setRetries(retries - 1);
       // Fetch the results once the countdown is over
       const missingSeconds = new Date(result.schedule_date).getTime() - new Date().getTime();
       const timer = setTimeout(() => loadData(), missingSeconds);
       return () => clearTimeout(timer);
     }
-  }, [loadData, result]);
-  // TODO loadData is a function, and this is making this hook to run several times (aparently is not able to check whether it changed)
+  }, [loadData, result, retries]);
 }
