bf5c999136422ae4247f02726cd09c5fb96733a3
David Naranjo <dnaranjo89@gmail.com>
2020-04-22 21:39:37 +0200

    Fix trailing / issues

--- pages/_error.jsx
+++ pages/_error.jsx
@@ -1,10 +1,24 @@
-const Error2 = ({ statusCode }) => (
-  <p>{statusCode ? `An error ${statusCode} occurred on server` : 'An error occurred on client'}</p>
-);
+import Error from 'next/error';
+import Router from 'next/router';
 
-Error2.getInitialProps = async ({ res, err }) => {
+export default Error;
+
+Error.getInitialProps = ({ res, err, asPath }) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-  return { statusCode, namespacesRequired: ['homepage'] };
-};
 
-export default Error2;
+  if (statusCode && statusCode === 404) {
+    if (asPath.match(/\/$/)) {
+      const withoutTrailingSlash = asPath.substr(0, asPath.length - 1);
+      if (res) {
+        res.writeHead(302, {
+          Location: withoutTrailingSlash,
+        });
+        res.end();
+      } else {
+        Router.push(withoutTrailingSlash);
+      }
+    }
+  }
+
+  return { statusCode };
+};
