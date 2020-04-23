c2dfb8af17a13a3f9005f90e78a9a7cca8594665
David Naranjo <dnaranjo89@gmail.com>
2020-04-23 22:56:12 +0200

    Fix some translations

--- i18n.js
+++ i18n.js
@@ -1,9 +1,8 @@
 const NextI18Next = require('next-i18next').default;
 
 const NextI18NextInstance = new NextI18Next({
-  defaultLanguage: 'es_ES',
-  otherLanguages: ['en_GB'],
-  // ignoreRoutes: ['/_next/', '/static/', 'robots.txt'],
+  defaultLanguage: 'es-ES',
+  otherLanguages: ['en-GB'],
 });
 
 module.exports = NextI18NextInstance;
