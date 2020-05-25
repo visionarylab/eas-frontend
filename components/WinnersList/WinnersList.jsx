ff80f126aef94de037adf9b9f3fe513cd86dcd29
David Naranjo <dnaranjo89@gmail.com>
2020-05-25 20:21:23 +0200

    Set labels to the right column in the Winners list

--- components/WinnersList/WinnersList.jsx
+++ components/WinnersList/WinnersList.jsx
@@ -12,10 +12,10 @@ const WinnersList = ({ winners, t }) => (
   <div className={c('WinnersList')} data-testid="WinnersList">
     <div className={STYLES.Titles}>
       <Typography className={STYLES.Title} variant="h3">
-        {t('winner_label')}
+        {t('prize_label')}
       </Typography>
       <Typography className={STYLES.Title} variant="h3">
-        {t('prize_label')}
+        {t('winner_label')}
       </Typography>
     </div>
     {winners.map(winner => (
