98a0f9983def9ebc08b10e0b1085e79de38dda52
David Naranjo <dnaranjo89@gmail.com>
2020-06-12 15:50:44 +0200

    Fix Recent Draws button in header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import Typography from '@material-ui/core/Typography';
-// import { Link as RouterLink } from 'react-router-dom';
-// import Link from '@material-ui/core/Link';
+import { useRouter } from 'next/router';
+import Button from '../Button.jsx';
 import { withTranslation } from '../../i18n';
 
 import STYLES from './Header.module.scss';
@@ -11,23 +11,30 @@ import logo from './logo_vector.svg';
 
 const c = classNames.bind(STYLES);
 
-const Header = ({ t }) => (
-  <header className={c('Header')}>
-    <a href="/" className={c('Header__link')}>
-      <img className={c('Header__logo')} src={logo} alt={t('brand_name')} />
-      <Typography variant="h4" component="span" className={c('Header__title')}>
-        {t('brand_name')}
-      </Typography>
-    </a>
-    {/* <span className={c('Header__links')}>
-      <Link component={RouterLink} to="/recent" color="textPrimary">
-        <Typography variant="body1" component="span" inline>
-          {t('recent_draws')}
+const showRecentDrawsEnabled = false;
+
+const Header = ({ t }) => {
+  const router = useRouter();
+  return (
+    <header className={c('Header')}>
+      <a href="/" className={c('Header__link')}>
+        <img className={c('Header__logo')} src={logo} alt={t('brand_name')} />
+        <Typography variant="h4" component="span" className={c('Header__title')}>
+          {t('brand_name')}
         </Typography>
-      </Link>
-    </span> */}
-  </header>
-);
+      </a>
+      {showRecentDrawsEnabled && router.pathname !== '/recent' && (
+        <span className={c('Header__links')}>
+          <Button href="/recent" color="textPrimary">
+            <Typography variant="body1" component="span" inline>
+              {t('recent_draws')}
+            </Typography>
+          </Button>
+        </span>
+      )}
+    </header>
+  );
+};
 
 Header.propTypes = {
   t: PropTypes.func.isRequired,
