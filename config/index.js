5832cf59e062cd43ba213ae5781e46d4aa96693c
David Naranjo <dnaranjo89@gmail.com>
2020-12-28 19:34:20 +0100

    Use Sentry alternative account

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ const environment = process.env.APP_ENV;
 
 try {
   const baseConfig = {
-    sentryDsn: 'https://5062c00c390b4ebc8f3239f85aec1054@o151502.ingest.sentry.io/5571829',
+    sentryDsn: 'https://bebd8f08ca1e44b0bd2b2d5f352332f4@o170509.ingest.sentry.io/1247679',
   };
 
   const environmentConfig = require(`./${environment}`); // eslint-disable-line
